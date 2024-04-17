07413948f40cad9964c148f4f55103f85eb9485a
Krakowiak Waldemar <144521687+mzkwcim@users.noreply.github.com>
2024-04-17 21:48:16 +0200

    Update PdfDataExtractor.cs

--- TestStrony/PdfExtractor/PdfDataExtractor.cs
+++ TestStrony/PdfExtractor/PdfDataExtractor.cs
@@ -12,38 +12,38 @@ namespace TestStrony.PdfExtractor
     {
         public static void Creater(string pdfFilePath)
         {
-            List<string> nazwy = ["RudolphTable8YearsOldBoys", "RudolphTable9YearsOldBoys", "RudolphTable10YearsOldBoys", "RudolphTable11YearsOldBoys", "RudolphTable12YearsOldBoys", "RudolphTable13YearsOldBoys", "RudolphTable14YearsOldBoys", "RudolphTable15YearsOldBoys", "RudolphTable16YearsOldBoys", "RudolphTable17YearsOldBoys", "RudolphTable18YearsOldBoys", "RudolphTableOpenBoys", "RudolphTable8YearsOldGirls", "RudolphTable9YearsOldGirls", "RudolphTable10YearsOldGirls", "RudolphTable11YearsOldGirls", "RudolphTable12YearsOldGirls", "RudolphTable13YearsOldGirls", "RudolphTable14YearsOldGirls", "RudolphTable15YearsOldGirls", "RudolphTable16YearsOldGirls", "RudolphTable17YearsOldGirls", "RudolphTable18YearsOldGirls", "RudolphTableOpenGirls",];
-            List<List<double>> listalist = [];
+            List<string> NamesOfTables = ["RudolphTable8YearsOldBoys", "RudolphTable9YearsOldBoys", "RudolphTable10YearsOldBoys", "RudolphTable11YearsOldBoys", "RudolphTable12YearsOldBoys", "RudolphTable13YearsOldBoys", "RudolphTable14YearsOldBoys", "RudolphTable15YearsOldBoys", "RudolphTable16YearsOldBoys", "RudolphTable17YearsOldBoys", "RudolphTable18YearsOldBoys", "RudolphTableOpenBoys", "RudolphTable8YearsOldGirls", "RudolphTable9YearsOldGirls", "RudolphTable10YearsOldGirls", "RudolphTable11YearsOldGirls", "RudolphTable12YearsOldGirls", "RudolphTable13YearsOldGirls", "RudolphTable14YearsOldGirls", "RudolphTable15YearsOldGirls", "RudolphTable16YearsOldGirls", "RudolphTable17YearsOldGirls", "RudolphTable18YearsOldGirls", "RudolphTableOpenGirls",];
+            List<List<double>> listOfLists = [];
             List<string> collumnNames = ["Punkty", "Freestyle_50m", "Freestyle_100m", "Freestyle_200m", "Freestyle_400m", "Freestyle_800m", "Freestyle_1500m", "Breaststroke_50m", "Breaststroke_100m", "Breaststroke_200m", "Butterfly_50m", "Butterfly_100m", "Butterfly_200m", "Backstroke_50m", "Backstroke_100m", "Backstroke_200m", "Medley_200m", "Medley_400m"];
-            for (int i = 0; i < nazwy.Count; i++)
+            for (int i = 0; i < NamesOfTables.Count; i++)
             {
-                listalist.Add([]);
+                listOfLists.Add([]);
             }
             List<string> pages = GetTextFromPdf(pdfFilePath);
-            int adder = 0, inter = 0;
+            int wordCounter = 0, pageCounter = 0; //these are support ints 
             foreach (string page in pages)
             {
-                List<string> w = [.. page.Split("\n")];
-                for (int i = 0; i < w.Count; i++)
+                List<string> row = [.. page.Split("\n")];
+                for (int i = 0; i < row.Count; i++)
                 {
-                    List<string> one = [.. w[i].Split(" ")];
-                    foreach (string word in one)
+                    List<string> words = [.. row[i].Split(" ")];
+                    foreach (string word in words)
                     {
                         if (word.Contains(':'))
                         {
-                            listalist[inter].Add(DataFormat.ConvertTimeStringToDouble(word.Replace(",", ".")));
-                            if ((adder + 1) % 340 == 0)
+                            listOfLists[pageCounter].Add(DataFormat.ConvertTimeStringToDouble(word.Replace(",", ".")));
+                            if ((wordCounter + 1) % 340 == 0)
                             {
-                                inter++;
+                                pageCounter++;
                             }
-                            adder++;
+                            wordCounter++;
                         }
                     }
                 }
             }
-            for (int i = 0; i < nazwy.Count; i++)
+            for (int i = 0; i < NamesOfTables.Count; i++)
             {
-                SqlDataManager.Connection(nazwy[i], collumnNames, listalist[i]);
+                SqlDataManager.Connection(NamesOfTables[i], collumnNames, listOfLists[i]);
             }
         }
         public static List<string> GetTextFromPdf(string pdfFilePath)
